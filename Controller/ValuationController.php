d532a259cf51d519d6b98b4f1096b9aa820c16e8
Miguel Mira Flor <mflor@solucionesaiapps.com>
2016-12-14 17:02:56 +0100

    remove comments

--- Controller/ValuationController.php
+++ Controller/ValuationController.php
@@ -26,14 +26,6 @@ class ValuationController extends Controller
         $email = $this->kernel->getContainer()->getParameter('aiapps_valuation_form.parameters.email');
         $terms = $this->kernel->getContainer()->getParameter('aiapps_valuation_form.parameters.terms');
 
-//        $apikey = $this->kernel->getContainer()->getParameter('valuation.form.apikey');
-//        $email = $this->kernel->getContainer()->getParameter('valuation.form.email');
-//        $terms = $this->kernel->getContainer()->getParameter('valuation.form.terms');
-
-//        $apikey = '9f83d4db086f23069633127ef77688da9a5ed496';
-//        $email = 'mmira@staiapps.com';
-//        $terms = 'http://staiapps.com/es/aviso-legal/';
-
         $template = $this->templating->renderResponse('AiappsValuationFormBundle:Default:index.html.twig', array(
             'apikey' => $apikey,
             'email' => $email,
